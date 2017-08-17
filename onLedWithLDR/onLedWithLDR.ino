e61c2e49e55bac17e10639d4aca3ebd2e7f896b0
= <=>
2017-08-16 22:20:29 -0300

    onLedWithLDR

--- onLedWithLDR/onLedWithLDR.ino
+++ onLedWithLDR/onLedWithLDR.ino
@@ -1,11 +1,18 @@
 const int LDR = A2;
+const int LED = 9;
 
 void setup() {  
   pinMode(LDR, INPUT);
+  pinMode(LED, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   int valor = analogRead(LDR);
   Serial.println(valor);
+  if(valor < 250){
+    digitalWrite(LED, HIGH);
+  }else{
+    digitalWrite(LED, LOW);
+  }
 }
